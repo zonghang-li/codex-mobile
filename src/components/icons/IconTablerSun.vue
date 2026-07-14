bee47e2c32924e4804383a0741555c9cde4fe02c
zonghangli <lizhuestc@gmail.com>
2026-07-15 03:08:03 +0400

    feat: add mobile dark mode shortcut

---
+++ src/components/icons/IconTablerSun.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" aria-hidden="true">
+    <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+      <path d="M12 3v1m0 16v1M3 12h1m16 0h1M5.64 5.64l.7.7m11.32 11.32l.7.7m0-12.72l-.7.7M6.34 17.66l-.7.7" />
+      <circle cx="12" cy="12" r="4" />
+    </g>
+  </svg>
+</template>
